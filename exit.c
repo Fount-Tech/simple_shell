c0870eae02a8d9ecdc3a2ae96dc668199dc5143d
Fount-Tech <fountech@gmail.com>
2022-12-21 15:04:20 -0800

    exit

---
+++ exit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *func_exit - function for exiting the program
+ *@cmd: comand entered by user
+ *@args: array of arguments
+ *@env: array of strings of env
+ *Return: 1 for success
+ */
+int func_exit(char *cmd, char **args, char **env)
+{
+	(void)cmd;
+	(void)args;
+	(void)env;
+	return (1);
+}
