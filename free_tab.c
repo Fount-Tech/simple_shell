32bacb1c034149e0586de6cc8e80c9439d661f54
Imeh Victor <imohimeh33@gmail.com>
2022-12-21 15:12:26 -0800

    A few files foe simple shell task

---
+++ free_tab.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * free_tab - free table of tokens
+ * @tabtok: table of token.
+ */
+void free_tab(char **tabtok)
+{
+	int i = 0;
+
+	if (tabtok != NULL)
+	{
+		while (tabtok[i])
+		{
+			free(tabtok[i]);
+			i++;
+		}
+		free(tabtok);
+	}
+
---
+++ main.h
@@ -0,0 +1,38 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define IN 1
+#define OUT 0
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+extern char **environ;
+
+typedef struct control
+{
+	char *saisi;
+	int (*f)(char *cmd, char **args, char **env);
+} control_t;
+
+
+void print_pr(void);
+int start(char *buff, char **env, int line);
+int (*control_saisii(char *saisi))(char *cmd, char **args, char **env);
+void free_tab(char **tabtok);
+int exec_path(char **args, char **env, int line);
+char *path_func(char *cmd, char *PATH);
+int func_exit(char *cmd, char **args, char **env);
+char **func_tok(char *buf);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+
+#endif
